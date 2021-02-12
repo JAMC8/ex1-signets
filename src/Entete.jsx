237e24f9b3a45a7ce750d20a64da7b551bf5ed7f
Julian Maldonado <juliandresmc@hotmail.com>
2021-02-11 22:24:04 -0500

    Création Entete

---
+++ src/Entete.jsx
@@ -0,0 +1,12 @@
+import './Entete.scss';
+
+export default function Entete(props)
+{
+    return(
+        <header className="Entete">
+            <div className="titre">Signets</div>
+            <div className="nom">Julian Maldonado</div>
+            <div className="imgProfil">ImgProfil</div>
+        </header>
+    )
+}
