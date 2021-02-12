0e58264934da4a13036bc53655b631ed329f651d
Julian Maldonado <juliandresmc@hotmail.com>
2021-02-11 22:22:22 -0500

    Création Entete

--- src/Appli.jsx
+++ src/Appli.jsx
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './Appli.scss';
+import Entete from './Entete';
 
 function Appli() {
   return (
     <div className="Appli">
+      <Entete/>
+      <section className="contenuPrincipal">
+        
+      </section>
       
     </div>
   );
